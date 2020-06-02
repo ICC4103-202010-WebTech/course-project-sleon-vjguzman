865e80a1ff9620d2e95d66f078053f63aa5a00c9
Santiago <sleon@miuandes.cl>
2020-06-02 02:53:43 -0400

    Jeepeta remix

---
+++ test/system/multimedia_test.rb
@@ -0,0 +1,41 @@
+require "application_system_test_case"
+
+class MultimediaTest < ApplicationSystemTestCase
+  setup do
+    @multimedium = multimedia(:one)
+  end
+
+  test "visiting the index" do
+    visit multimedia_url
+    assert_selector "h1", text: "Multimedia"
+  end
+
+  test "creating a Multimedium" do
+    visit multimedia_url
+    click_on "New Multimedium"
+
+    click_on "Create Multimedium"
+
+    assert_text "Multimedium was successfully created"
+    click_on "Back"
+  end
+
+  test "updating a Multimedium" do
+    visit multimedia_url
+    click_on "Edit", match: :first
+
+    click_on "Update Multimedium"
+
+    assert_text "Multimedium was successfully updated"
+    click_on "Back"
+  end
+
+  test "destroying a Multimedium" do
+    visit multimedia_url
+    page.accept_confirm do
+      click_on "Destroy", match: :first
+    end
+
+    assert_text "Multimedium was successfully destroyed"
+  end
+end
