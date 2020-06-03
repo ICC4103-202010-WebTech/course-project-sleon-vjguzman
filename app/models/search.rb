f53374cd167a073d848f4b22c8241522d7912de1
vjguzman <vjguzman@miuandes.cl>
2020-06-02 23:25:34 -0400

    :((

--- app/models/search.rb
+++ app/models/search.rb
@@ -4,10 +4,18 @@ class Search < ApplicationRecord
     @users ||= find_users
   end
 
+  def events
+    @events ||= find_events
+  end
+
   private
   def find_users
-    users = User.order(:name)
-    users_name = users.where("full_name like?", "%#{keywords}%") if keywords.present?
-    users_username = users.where("username like ?", "%#{keywords}%") if keywords.present?
+    @users = User.order(:name)
+    @users= users.where("full_name like?", "%#{keywords}%") if keywords.present?
+    @users = users.where("username like ?", "%#{keywords}%") if keywords.present?
+  end
+
+  def find_events
+
   end
 end
