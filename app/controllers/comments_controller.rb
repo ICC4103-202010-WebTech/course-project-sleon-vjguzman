da7510acab721ffff4629a949d7a513ed67d3a7a
vjguzman <vjguzman@miuandes.cl>
2020-06-17 17:14:43 -0400

    arreglos

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -31,8 +31,8 @@ class CommentsController < ApplicationController
 
     respond_to do |format|
       if @comment.save
-        format.html { redirect_to @comment, notice: 'Comment was successfully created.' }
-        format.json { render :show, status: :created, location: @comment }
+        format.html { redirect_to comments_url, notice: 'Comment was successfully created.' }
+        format.json { render comments_url, status: :created, location: @comment }
       else
         format.html { render :new }
         format.json { render json: @comment.errors, status: :unprocessable_entity }
@@ -45,8 +45,8 @@ class CommentsController < ApplicationController
   def update
     respond_to do |format|
       if @comment.update(comment_params)
-        format.html { redirect_to @comment, notice: 'Comment was successfully updated.' }
-        format.json { render :show, status: :ok, location: @comment }
+        format.html { redirect_to comments_url, notice: 'Comment was successfully updated.' }
+        format.json { render comments_url, status: :ok, location: @comment }
       else
         format.html { render :edit }
         format.json { render json: @comment.errors, status: :unprocessable_entity }
