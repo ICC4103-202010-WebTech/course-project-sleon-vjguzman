b951d4ec1aa0210547ac4a4bc321cdfcc22ea3fe
Santiago <sleon@miuandes.cl>
2020-06-23 19:15:30 -0400

    Google Log in Ready papurri

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
   devise_for :admins, controllers: { sessions: 'admins/sessions' }
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root "pages#home"
   namespace :api, defaults: {format: :json} do
