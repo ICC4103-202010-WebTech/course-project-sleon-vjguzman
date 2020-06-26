644fd1091cfa148799ed1668f760ff3350611ce6
Santiago <sleon@miuandes.cl>
2020-06-26 14:33:47 -0400

    Vicho carrea

--- app/models/event.rb
+++ app/models/event.rb
@@ -11,7 +11,7 @@ class Event < ApplicationRecord
   has_many :Notifications, dependent: :destroy
   validates :choice_of_date_id, presence:true
   validates :privacy_id, presence:true
-  validates :organization_id, presence:true
+
   has_one_attached :banner_picture
   has_many_attached :images
   has_many_attached :files
